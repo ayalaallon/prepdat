b838d7f6cff45963205cd42f82466b739ee0e3b3
Tal Galili <tal.galili@gmail.com>
2016-09-23 01:15:34 +0300

    Tests code should be placed inside test_that

--- tests/testthat/test_prep.R
+++ tests/testthat/test_prep.R
@@ -1,37 +1,41 @@
 skip_on_cran()
-library(prepdat)
-data("finalized_stroopdata")
-data("stroopdata")
 
 context("Finalized table")
 
-test_finalized_stroopdata <- prep(
-  dataset = stroopdata
-  , file_name = NULL
-  , file_path = NULL
-  , id = "subject"
-  , within_vars = c("block", "target_type")
-  , between_vars = c("order")
-  , dvc = "rt"
-  , dvd = "ac"
-  , keep_trials = NULL
-  , drop_vars = c()
-  , keep_trials_dvc = "raw_data$rt > 100 & raw_data$rt < 3000 & raw_data$ac == 1"
-  , keep_trials_dvd = "raw_data$rt > 100 & raw_data$rt < 3000"
-  , id_properties = c()
-  , sd_criterion = c(1, 1.5, 2)
-  , percentiles = c(0.05, 0.25, 0.75, 0.95)
-  , outlier_removal = 2
-  , keep_trials_outlier = "raw_data$ac == 1"
-  , decimal_places = 0
-  , notification = TRUE
-  , dm = c()
-  , save_results = FALSE
-  , results_name = "results.txt"
-  , results_path = NULL
-  , save_summary = FALSE
-)
+
 
 test_that("Finialized table is correct", {
+  
+  library(prepdat)
+  data("finalized_stroopdata")
+  data("stroopdata")
+  
+  test_finalized_stroopdata <- prep(
+    dataset = stroopdata
+    , file_name = NULL
+    , file_path = NULL
+    , id = "subject"
+    , within_vars = c("block", "target_type")
+    , between_vars = c("order")
+    , dvc = "rt"
+    , dvd = "ac"
+    , keep_trials = NULL
+    , drop_vars = c()
+    , keep_trials_dvc = "raw_data$rt > 100 & raw_data$rt < 3000 & raw_data$ac == 1"
+    , keep_trials_dvd = "raw_data$rt > 100 & raw_data$rt < 3000"
+    , id_properties = c()
+    , sd_criterion = c(1, 1.5, 2)
+    , percentiles = c(0.05, 0.25, 0.75, 0.95)
+    , outlier_removal = 2
+    , keep_trials_outlier = "raw_data$ac == 1"
+    , decimal_places = 0
+    , notification = TRUE
+    , dm = c()
+    , save_results = FALSE
+    , results_name = "results.txt"
+    , results_path = NULL
+    , save_summary = FALSE
+  )
+  
   expect_equal(test_finalized_stroopdata, finalized_stroopdata)
 })
